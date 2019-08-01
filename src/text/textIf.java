465e4470616522187b6c5b68447adb82d8afd379
清 阿 <QingShuiDouFu@outlook.com>
2019-08-01 16:08:54 +0800

    注释

--- src/text/textIf.java
+++ src/text/textIf.java
@@ -2,7 +2,9 @@ package text;
 //测试if单选结构
 public class textIf {
 	public static void main (String[] args) {
-		//double d=Math.random();  //返回0到1的随机数
+		//double d=Math.random();
+
+		// 返回0到1的随机数
 		//System.out.println(d);
 		int i=(int)(6*Math.random())+1;
 		System.out.println(i);
@@ -14,3 +16,6 @@ public class textIf {
 			System.out.println("大");
 	}
 }
+
+//注释测试
+
