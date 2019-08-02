a509a2f8f141116c9965b04d25166576272181e0
清 阿 <QingShuiDouFu@outlook.com>
2019-08-03 00:56:58 +0800

    注释

---
+++ src/test/testIf.java
@@ -0,0 +1,21 @@
+package test;
+//测试if单选结构
+public class testIf {
+	public static void main (String[] args) {
+		//double d=Math.random();
+
+		// 返回0到1的随机数s
+		//System.out.println(d);
+		int i=(int)(6*Math.random())+1;
+		System.out.println(i);
+		if (i<=3) {
+			System.out.println("小");
+			
+		}
+		else 
+			System.out.println("大");
+	}
+}
+
+//注释测试
+
