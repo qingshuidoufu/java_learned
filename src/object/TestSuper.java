adfd80e930a09d189d1abf796bdf00eca04d5047
清 阿 <QingShuiDouFu@outlook.com>
2019-08-19 14:42:21 +0800

    测试super方法

---
+++ src/object/TestSuper.java
@@ -0,0 +1,29 @@
+package object;
+
+public class TestSuper {
+    public static void main (String[] args){
+    new ChildClass().f();
+
+    }
+}
+class FatherClass{
+    public int value;
+
+    public void f(){
+        value=100;
+        System.out.println("FatherClass.value="+value);
+        }
+}
+
+class ChildClass extends FatherClass{
+    public int value;
+    public void f(){
+        super.f();//调用父类的对象成员普通方法
+        value=200;
+        System.out.println("ChildClass.value="+value);
+        System.out.println(value);
+        System.out.println(super.value);//调用父类成员的成员变量
+
+    }
+}
+
