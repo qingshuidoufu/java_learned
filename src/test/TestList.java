d3324cd1b06b6048bbe9628bee6f9f7990303261
清 阿 <QingShuiDouFu@outlook.com>
2019-09-07 15:33:58 +0800

    测试List的方法

--- src/test/TestList.java
+++ src/test/TestList.java
@@ -1,33 +1,64 @@
 package test;
 
+import arrys.TestArrys01;
 import com.sun.scenario.effect.impl.sw.java.JSWBlend_SRC_OUTPeer;
 
 import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 //测试collection接口的方法
 public class TestList {
     public static void main(String[] args) {
-        Collection<String> c=new ArrayList<>();
-        System.out.println(c.size());
-        System.out.println(c.isEmpty());
+                       test02();
+                       test03();
 
-        c.add("jack");
-        c.add("marry");
-        System.out.println(c);
-        System.out.println(c.size());
-        Object[] objs=c.toArray();
-        System.out.println(c.contains("jack"));
 
-        System.out.println(objs);
+    }
+    public static void Test01(){
 
-        c.remove("jack");
-        System.out.println(c);
-        c.clear();
-        System.out.println(c.size());
+
+    }
+    public static void test02(){
+        List<String> list01=new ArrayList<>();
+        list01.add("aa");
+        list01.add("bb");
+        list01.add("cc");
+        List<String> list02=new ArrayList<>();
+        list02.add("aa");
+        list02.add("dd");
+        list02.add("ee");
+        System.out.println("list01:"+list01);
+       // list01.addAll(list02);
+       // list01.removeAll(list02);
+        list01.retainAll(list02);
+        System.out.println("list01:"+list01);
+        System.out.println(list01.containsAll(list02));
 
 
 
     }
+    public static void test03(){
+        List<String> list =new ArrayList<>();
+        list.add("A");
+        list.add("B");
+        list.add("C");
+        list.add("D");
+        System.out.println(list);
+        list.add(2,"jack");
+        System.out.println(list);
+        list.remove(2);
+        System.out.println(list);
+        list.set(2,"marry");
+        System.out.println(list);
+        System.out.println(list.get(2));
+        list.add("C");
+        list.add("B");
+        list.add("A");
+        System.out.println(list);
+        System.out.println(list.indexOf("B"));
+        System.out.println(list.lastIndexOf("B"));
+    }
+
 }
