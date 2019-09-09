38e9f128dd51131eccf44b3b0e156853e917796a
清 阿 <QingShuiDouFu@outlook.com>
2019-09-09 19:20:08 +0800

    手写ArrayList,索引检查

--- src/myCollection/MyArrayList.java
+++ src/myCollection/MyArrayList.java
@@ -1,4 +1,7 @@
 package myCollection;
+
+import javax.management.RuntimeErrorException;
+
 //自定义一个ArrayList,体会底层
 public class MyArrayList <E>{
     private Object [] elementData;
@@ -26,12 +29,41 @@ public class MyArrayList <E>{
         this.elementData = new Object[capacity];
     }
     public void add (E e){
+        //什么时候扩容
+        if (size==elementData.length){
+            //怎么扩容
+            Object[] newArray=new Object[elementData.length+(elementData.length>>1)];//10+5=15
+            System.arraycopy(elementData,0,newArray,0,elementData.length);
+            elementData=newArray;
+
+    }
+
         elementData[size++]=e;
+
+    }
+    public E get(int index){
+        checkRange(index);
+        return (E)elementData[index];
+
+    }
+    public void set(E element,int index){
+        checkRange(index);
+        elementData[index]=element;
+
+    }
+    public void checkRange(int index){
+        if (index<0||index>size-1){
+            throw new RuntimeException("索引不合法:"+index);
+        }
     }
     public static void main(String[] args) {
         MyArrayList s1=new MyArrayList(20);
-        s1.add("aa");
-        s1.add("bb");
+        for (int i = 0; i <40 ; i++) {
+        s1.add("jack"+i);
+        }
         System.out.println(s1);
+        s1.set("gcgcgc",10);
+        System.out.println(s1.get(10));
+
     }
 }
