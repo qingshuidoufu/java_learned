9f7eb21cf376bac286f916bd087ae33de7fbff2c
清 阿 <QingShuiDouFu@outlook.com>
2019-09-12 13:27:36 +0800

    testVector

---
+++ src/test/TestVector.java
@@ -0,0 +1,10 @@
+package test;
+
+import java.util.List;
+import java.util.Vector;
+
+public class TestVector {
+    public static void main(String[] args) {
+        List<String> a=new Vector<>();
+    }
+}
