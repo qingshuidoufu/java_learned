dd333baf94c2868adb89e84e068abb7f0229e478
清 阿 <QingShuiDouFu@outlook.com>
2019-09-12 16:53:28 +0800

    重写toString

--- src/map/HashMap.java
+++ src/map/HashMap.java
@@ -6,6 +6,23 @@ import test.Node;
 public class HashMap {
         Node2[] table;  //位桶数组. bucket array
         int size;       //键值对的个数
+
+    @Override
+    public String toString() {
+        //{10:aa,20:bb}
+        StringBuilder sb=new StringBuilder("{");
+
+        for (int i = 0; i < table.length; i++) {
+            Node2 temp=table[i];
+            while (  temp!=null){
+                sb.append(temp.key+":"+temp.value+",");
+                temp=temp.next;
+            }
+        }
+        sb.setCharAt(sb.length()-1,'}');
+        return sb.toString();
+    }
+
     public HashMap(){
         table =new Node2[16];  //长度一般定义成2的整数次幂
     }
@@ -27,7 +44,7 @@ public class HashMap {
             while(temp!=null) {
                 //判断key如果重复,则覆盖
                 if(temp.key.equals(key)){
-                    System.out.println("key重复了");
+                    //System.out.println("key重复了");
                     temp.value=value;//只是覆盖value即可,hash,key,next不变
                     keyRepeat=true;
                     break;
@@ -46,8 +63,8 @@ public class HashMap {
 
     }
     public  static int myHash(int v,int length){
-        System.out.println("hash in myHash"+(v&(length-1)));//按位与,效率高(要求长度为2的整数次幂)
-        System.out.println("hash in myHash"+(v%(length-1)));//效率低
+       // System.out.println("hash in myHash"+(v&(length-1)));//按位与,效率高(要求长度为2的整数次幂)
+        //System.out.println("hash in myHash"+(v%(length-1)));//效率低
 
         return v&(length-1);
     }
@@ -62,9 +79,7 @@ public class HashMap {
         m.put(69,"hh");
         m.put(85,"kk");
         System.out.println(m);
-        for (int i = 10; i <100 ; i++) {
-            System.out.println(i+"----"+myHash(i,16));//53,69,85
-        }
+
 
     }
 }
